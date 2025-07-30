76a7c8ab2a87d4df3fd3d27b3365c0d0c79c62f9
Kyle <kb0531@cs.washington.edu>
2025-07-30 20:58:39 +0900

    added logos to coins and fetched data from home screen through useLocation

--- src/routes/Coin.tsx
+++ src/routes/Coin.tsx
@@ -1,15 +1,54 @@
-import { useParams } from "react-router";
+import { useParams, useLocation } from "react-router";
+import styled from 'styled-components';
+import { useState } from "react";
+
+const Container = styled.div`
+  padding: 0px 20px; // padding: vertically horizontally
+  max-width: 480px;
+  margin: 0 auto; // top & bottom margins are set to 0, left & right margins are automatically calculated by the browser
+`;
+
+const Header = styled.header`
+  height: 10vh;
+  display: flex; 
+  justify-content: center;
+  align-itmes: center;
+  margin-top: 10px;
+`;
+
+const Title = styled.h1`
+  font-size: 48px;
+  color: ${props => props.theme.accentColor};
+`;
+
+const Loader = styled.div`
+  text-align: center;
+`;
 
 // alternative way to instiantiate type for typescript
-interface Params {
+interface RouteParams {
     coinId: string;
 };
 
+interface RouteState {
+    name: string;
+}
+
 function Coin() {
     // const { coinId } = useParams<{coinId:string}>();
-    const { coinId } = useParams<Params>();
+    const { coinId } = useParams<RouteParams>();
+    const [loading, setLoading] = useState(true);
+    const { state } = useLocation<RouteState>();
+
+    // if you come directly to localhost:3000/btc (for e.g.) Title will be shown as "Loading"
+    // but if you come through the home page the Title will be displayed as "Bitcoin"
     return (
-        <h1>Coin: {coinId}</h1>
+        <Container>
+            <Header>
+                <Title>{state?.name || "Loading"}</Title>
+            </Header>
+            {loading ? <Loader>Loading...</Loader> : null}
+        </Container>
     );
 }
 
--- src/routes/Coins.tsx
+++ src/routes/Coins.tsx
@@ -1,9 +1,12 @@
 import styled from 'styled-components';
 import { Link } from "react-router-dom"; // we use Link because "a href=" refreshes the page
+import { useState, useEffect } from "react";
 
 // padding => inner html ; margin => outer html
 const Container = styled.div`
-  padding: 0px 20px // padding: vertically horizontally
+  padding: 0px 20px; // padding: vertically horizontally
+  max-width: 480px;
+  margin: 0 auto; // top & bottom margins are set to 0, left & right margins are automatically calculated by the browser
 `;
 
 const Header = styled.header`
@@ -12,11 +15,11 @@ const Header = styled.header`
   justify-content: center;
   align-itmes: center;
   margin-top: 10px;
-  margin-bottom: 30px;
 `;
 
 const CoinsList = styled.ul``;
 
+// display: block; // allows user to click anywhere on the container not just where the link is
 const Coin = styled.li`
   background-color: white;
   color: ${props => props.theme.bgColor};
@@ -24,8 +27,9 @@ const Coin = styled.li`
   border-radius: 15px;
   a {
     transition: color 0.2s ease-in;
-    display: block; // allows user to click anywhere on the container not just where the link is
-      padding: 20px;
+    padding: 20px;
+    display: flex;
+    align-items: center;
   }
   &:hover {
     a {
@@ -39,50 +43,55 @@ const Title = styled.h1`
   color: ${props => props.theme.accentColor};
 `;
 
-const coins = [
-  {
-    id: "btc-bitcoin",
-    name: "Bitcoin",
-    symbol: "BTC",
-    rank: 1,
-    is_new: false,
-    is_active: true,
-    type: "coin",
-  },
-  {
-    id: "eth-ethereum",
-    name: "Ethereum",
-    symbol: "ETH",
-    rank: 2,
-    is_new: false,
-    is_active: true,
-    type: "coin",
-  },
-  {
-    id: "hex-hex",
-    name: "HEX",
-    symbol: "HEX",
-    rank: 3,
-    is_new: false,
-    is_active: true,
-    type: "token",
-  },
-];
+const Loader = styled.div`
+  text-align: center;
+`;
+
+const Img = styled.img`
+  width: 35px;
+  height: 35px;
+  margin-right: 10px;
+`;
+
+interface CoinInterface {
+  id: string,
+  name: string,
+  symbol: string,
+  rank: number,
+  is_new: boolean,
+  is_active: boolean,
+  type: string,
+}
 
 function Coins() {
+  const [coins, setCoins] = useState<CoinInterface[]>([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    (async () => {
+      const json = await (await (fetch("https://api.coinpaprika.com/v1/coins"))).json();
+      setCoins(json.splice(0, 100));
+      setLoading(false);
+    })();
+  }, []);
+
   return (
     <Container>
       <Header>
         <Title>코인</Title>
       </Header>
-      <CoinsList>
+      {loading ? <Loader>Loading...</Loader> : <CoinsList>
         {coins.map(coin =>
           <Coin key={coin.id}>
-            <Link to={`/${coin.id}`}>
+            <Link to={{
+              pathname: `/${coin.id}`,
+              state: { name: coin.name }
+            }}>
+              <Img src={`https://cryptocurrencyliveprices.com/img/${coin.id}.png`} />
               {coin.name}&rarr;
             </Link>
           </Coin>)}
-      </CoinsList>
+      </CoinsList>}
     </Container>
   );
 }
