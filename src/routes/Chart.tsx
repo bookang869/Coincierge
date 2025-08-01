9408cb601d476b60e44ad6f2ba61f98caa56c0a8
Kyle <kb0531@cs.washington.edu>
2025-08-01 23:08:09 +0900

    created chart using coin history

--- src/routes/Chart.tsx
+++ src/routes/Chart.tsx
@@ -1,15 +1,92 @@
 import { useQuery } from "react-query";
 import { fetchCoinHistory } from "../api";
+import ApexChart from "react-apexcharts";
 
 interface ChartProps {
   coinId: string;
 }
 
+interface iData {
+  time_open: number;
+  time_close: number;
+  open: string;
+  high: string;
+  low: string;
+  close: string;
+  volume: string;
+  market_cap: number;
+}
+
 function Chart({ coinId }: ChartProps) {
-  const { isLoading, data } = useQuery(["ohlcv", coinId], () =>
+  const { isLoading, data } = useQuery<iData[]>(["ohlcv", coinId], () =>
     fetchCoinHistory(coinId)
   );
-  return <h1>CHART</h1>;
+  return (
+    <div>
+      {isLoading ? (
+        "Loading..."
+      ) : (
+        <ApexChart
+          type="line"
+          series={[
+            {
+              name: "Price",
+              // ?? [] prevents data from being null
+              data: data?.map((price) => parseFloat(price.close)) ?? [],
+            },
+          ]}
+          options={{
+            theme: { mode: "dark" },
+            chart: {
+              height: 300,
+              width: 500,
+              // hides toolbar option
+              toolbar: {
+                show: false,
+              },
+              background: "transparent",
+            },
+            stroke: { curve: "smooth", width: 3 },
+            grid: { show: false },
+            // remove x axis
+            xaxis: {
+              labels: {
+                show: false,
+              },
+              axisBorder: {
+                show: false,
+              },
+              axisTicks: {
+                show: false,
+              },
+              type: "datetime",
+              categories: data?.map((price) =>
+                new Date(price.time_close * 1000).toUTCString()
+              ),
+            },
+            // remove y-axis
+            yaxis: {
+              show: false,
+            },
+            // apply gradient to the color scheme of graph
+            fill: {
+              type: "gradient",
+              gradient: {
+                gradientToColors: ["blue"],
+                stops: [0, 100],
+              },
+            },
+            colors: ["red"],
+            tooltip: {
+              y: {
+                formatter: (value) => `$${value.toFixed(2)}`,
+              },
+            },
+          }}
+        />
+      )}
+    </div>
+  );
 }
 
 export default Chart;
